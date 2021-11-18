1b762601d769381826c524c1261e2d7483199273
parkdoyoung <ehdud5578@gmail.com>
2021-11-18 17:17:44 +0900

    add : class 1118 oracle sql

--- KosaJAVA/src/sql/class1117/kosa_1117.sql
+++ KosaJAVA/src/sql/class1117/kosa_1117.sql
@@ -1,53 +1,58 @@
-alter session set"_ORACLE_SCRIPT"=true;
+alter session set "_ORACLE_SCRIPT"= true;
 create user kosa identified by oracle;
-grant resuorce,create session,content,dba to kosa;
+grant resuorce, create session,content, dba to kosa;
 alter user kosa default tablespace users quota unlimited on users;
 
-select * from dba_users;
+select *
+from dba_users;
 
-CREATE TABLE EMPLOYEEs(
-    EMPLOYEE_ID NUMBER(6),
-    FIRST_NAME VARCHAR2(20),
-    LAST_NAME VARCHAR2(25),
-    EMAIL VARCHAR2(25),
-    PHONE_NUMBER VARCHAR2(20),
-    HIRE_DATE DATE,
-    JOB_ID VARCHAR2(10),
-    SALARY NUMBER(8,2),
-    COMMISION_PCT NUMBER(2,2),
-    MANAGER_ID NUMBER(6),
+CREATE TABLE EMPLOYEEs
+(
+    EMPLOYEE_ID   NUMBER(6),
+    FIRST_NAME    VARCHAR2(20),
+    LAST_NAME     VARCHAR2(25),
+    EMAIL         VARCHAR2(25),
+    PHONE_NUMBER  VARCHAR2(20),
+    HIRE_DATE     DATE,
+    JOB_ID        VARCHAR2(10),
+    SALARY        NUMBER(8, 2),
+    COMMISION_PCT NUMBER(2, 2),
+    MANAGER_ID    NUMBER(6),
     DEPARTMEMT_ID NUMBER(4)
 );
 
-create table departments(
-    DEPARTMENT_ID NUMBER(4) not null,
+create table departments
+(
+    DEPARTMENT_ID   NUMBER(4)    not null,
     department_name VARCHAR2(30) not null,
-    manager_id NUMBER,
-    LOCATION_ID NUMBER(4) default 1800
+    manager_id      NUMBER,
+    LOCATION_ID     NUMBER(4) default 1800
 );
 
 drop table departments;
 
-SELECT * FROM DEPARTMENTS;
+SELECT *
+FROM DEPARTMENTS;
 
 describe employees;
 
-alter table employee rename to employees;
 
-insert into departments(department_id,department_name,manager_id,location_id)
-values (300,'Engineering',110,1700);
+insert into departments(department_id, department_name, manager_id, location_id)
+values (300, 'Engineering', 110, 1700);
 
-insert into departments(department_id,department_name,manager_id)
-values (300,'Engineering',110);
+insert into departments(department_id, department_name, manager_id)
+values (300, 'Engineering', 110);
 
-select * from departments;
+select *
+from departments;
 
 drop table departments;
-create table departments(
-    DEPARTMENT_ID NUMBER(4) not null,
+create table departments
+(
+    DEPARTMENT_ID   NUMBER(4)    not null,
     department_name VARCHAR2(30) not null,
-    manager_id NUMBER,
-    LOCATION_ID NUMBER(4)
+    manager_id      NUMBER,
+    LOCATION_ID     NUMBER(4)
 );
 
 insert into departments(department_id)
@@ -55,126 +60,161 @@ values (300);
 
 drop table employees;
 
-create table employees(
-    employee_id number not null,
-    first_name varchar2(255),
-    last_name varchar2(255) not null,
-    email varchar2(255) not null,
-    phone varchar2(50) not null,
-    hire_date date not null,
-    manager_id varchar2(255),
-    job_id varchar2(255),
-    salary number(8,2),
-    commision_pct number(2,2),
+create table employees
+(
+    employee_id   number        not null,
+    first_name    varchar2(255),
+    last_name     varchar2(255) not null,
+    email         varchar2(255) not null,
+    phone         varchar2(50)  not null,
+    hire_date     date          not null,
+    manager_id    varchar2(255),
+    job_id        varchar2(255),
+    salary        number(8, 2),
+    commision_pct number(2, 2),
     department_id number(4)
 );
 
-insert into employees values(100,'doyoung','park','ehdud5578','010-1234-1234','1996-06-17',null,null,1000,null,10
-);
+insert into employees
+values (100, 'doyoung', 'park', 'ehdud5578', '010-1234-1234', '1996-06-17', null, null, 1000, null, 10);
 
-select * from employees;
+select *
+from employees;
 
 drop table employees;
 
-select sysdate from dual; -- 가상의 테이블에서 가져옴.
+select sysdate
+from dual; -- 가상의 테이블에서 가져옴.
 
-create table employees(
-    employee_id number not null,
-    first_name varchar2(255),
-    last_name varchar2(255) not null,
-    email varchar2(255) not null,
-    phone varchar2(50) not null,
-    hire_date date default sysdate not null,
-    manager_id varchar2(255),
-    job_id varchar2(255),
-    salary number(8,2),
-    commision_pct number(2,2),
+create table employees
+(
+    employee_id   number               not null,
+    first_name    varchar2(255),
+    last_name     varchar2(255)        not null,
+    email         varchar2(255)        not null,
+    phone         varchar2(50)         not null,
+    hire_date     date default sysdate not null,
+    manager_id    varchar2(255),
+    job_id        varchar2(255),
+    salary        number(8, 2),
+    commision_pct number(2, 2),
     department_id number(4)
 );
 
-insert into employees(employee_id,last_name,email,phone)
-values(101,'asdf','ehdud@asdf','010-1234-1234');
+insert into employees(employee_id, last_name, email, phone)
+values (101, 'asdf', 'ehdud@asdf', '010-1234-1234');
 
-select * from employees;
+select *
+from employees;
 commit;
 
-create table tb1(
+create table tb1
+(
     col1 varchar2(3),
     col2 varchar2(3 Byte),
     col3 varchar2(3 char)
 );
 
-insert into tb1(col1) values('abc');
-insert into tb1(col1) values('박도영');
-insert into tb1(col2) values('abc');
-insert into tb1(col2) values('박도영');
-insert into tb1(col3) values('abc');
-insert into tb1(col3) values('박도영');
+insert into tb1(col1)
+values ('abc');
+insert into tb1(col1)
+values ('박도영');
+insert into tb1(col2)
+values ('abc');
+insert into tb1(col2)
+values ('박도영');
+insert into tb1(col3)
+values ('abc');
+insert into tb1(col3)
+values ('박도영');
 
-select * from tb1;
+select *
+from tb1;
 
-create table tb2(
+create table tb2
+(
     col1 Integer,
     col2 decimal,
     col3 number
 );
 
-select * from user_tab_cols
-where table_name ='TB2';
+select *
+from user_tab_cols
+where table_name = 'TB2';
 
 
-create table tb3(
+create table tb3
+(
     col1 float(32),
     col2 float -- default 는 126 이다.
 );
 
-select * from user_tab_cols -- kosa - table - cols
-where table_name='TB3';
+select *
+from user_tab_cols -- kosa - table - cols
+where table_name = 'TB3';
 
-create table tb4(
+create table tb4
+(
     cal1 date,
     col2 timestamp
 );
 
-insert into tb4 values(sysdate,sysdate);
+insert into tb4
+values (sysdate, sysdate);
 
-select * from tb4;
+select *
+from tb4;
 
-insert into tb4 values(sysdate,systimestamp);
+insert into tb4
+values (sysdate, systimestamp);
 
-select * from tb4;
+select *
+from tb4;
 
-create table tb5(
+create table tb5
+(
     col1 number,
     col2 varchar2(20)
 );
 
-insert into tb5 values(1,'parkdoyoung');
-insert into tb5 values(1,'parkdohyun');
-select * from tb5 where col1 = 1;
+insert into tb5
+values (1, 'parkdoyoung');
+insert into tb5
+values (1, 'parkdohyun');
+select *
+from tb5
+where col1 = 1;
 
 -- 중복값을 허용하지 않음 unique 
 drop table tb5;
 
-create table tb5(
+create table tb5
+(
     col1 number unique,
     col2 varchar2(20)
 );
 
 
-create table tb9(
-    col1 number primary key,
-    email varchar2(20) unique,
-    phone varchar2(13) unique not null,
+create table tb9
+(
+    col1       number primary key,
+    email      varchar2(20) unique,
+    phone      varchar2(13) unique not null,
     first_name varchar2(20)
-);  
+);
 
-insert into tb9 values(1,'a','1234','firstname');
-insert into tb9 values(2,null,'1235','firstname');
-insert into tb9 values(3,null,'123','firstname');
-insert into tb9 values(3,'','1235','firstname');
-insert into tb9 values(4,'','','firstname');
-select * from tb9;
+insert into tb9
+values (1, 'a', '1234', 'firstname');
+insert into tb9
+values (2, null, '1235', 'firstname');
+insert into tb9
+values (3, null, '123', 'firstname');
+insert into tb9
+values (3, '', '1235', 'firstname');
+insert into tb9
+values (4, '', '', 'firstname');
+select *
+from tb9;
 drop table tb9;
 commit;
 
---
+++ KosaJAVA/src/sql/class1117/kosa_1118.sql
@@ -0,0 +1,195 @@
+drop table DEPARTMENTS;
+create table departments
+(
+    departmemt_id   number(4)
+        constraint dep_depid_pk primary key,
+    department_name varchar2(30) not null
+        constraint dep_depname_NM not null,
+    manager_id      number(6),
+    location_id     number(4)
+);
+create table departments
+(
+    departmemt_id   number(4),
+    department_name varchar2(30) not null,
+    manager_id      number(6),
+    location_id     number(4),
+    constraint dep_depid_pk primary key (departmemt_id)
+);
+
+insert into departments
+values (90, 'Executive', 100, 1700);
+
+drop table departments;
+
+create table departments
+(
+    departmemt_id   number(4),
+    department_name varchar2(30) not null,
+    manager_id      number(6) references EMPLOYEES (EMPLOYEE_ID),
+    location_id     number(4),
+    constraint dep_depid_pk primary key (departmemt_id)
+);
+
+drop table departments;
+
+create table departments
+(
+    department_id   number(4)
+        constraint dep_depid_pk primary key,
+    department_name varchar2(30) not null,
+    manager_id      number(6)
+        constraint dep_mngid_fk references EMPLOYEES (EMPLOYEE_ID),
+    location_id     number(4)
+);
+drop table departments;
+
+create table departments
+(
+    department_id   number(4),
+    department_name varchar2(30) not null,
+    manager_id      number(6),
+    location_id     number(4),
+    constraint dep_depid_pk primary key (department_id),
+    constraint dep_mngid_fk foreign key (manager_id)
+        references EMPLOYEES (EMPLOYEE_ID)
+);
+
+select *
+from employees;
+
+insert into departments
+values (90, 'executive', 100, 1700);
+
+create table c1
+(
+    c1 number check ( c1 > 0 ),
+    c2 number
+);
+
+insert into c1 (c1, c2)
+values (-1, 1);
+
+insert into c1 (c1, c2)
+values (1, 1);
+
+select *
+from c1;
+
+create table c2
+(
+    c1 number,
+    c2 number,
+    constraint c_c1_ck check ( c1 > 0 ),
+    constraint c_c2_ck check ( c2 > c1 )
+);
+
+insert into c2
+values (1, 2);
+select *
+from c2;
+
+select *
+from USER_CONSTRAINTS
+where TABLE_NAME = 'C2';
+
+create table c4
+(
+    c1 number check ( c1 between 1 and 100),
+    c2 varchar2(20) check ( c2 in ('a', 'b', 'c')),
+    c3 varchar2(20) check ( c3 in ('seoul', 'busan', 'daegu', 'daejeon') )
+    -- c4 varchar2(20) check ( c4 like '[a-f]%')
+);
+
+drop table c4;
+insert into c4
+values (10, 'a', 'seoul');
+
+create table c5
+(
+    c1 number,
+    c2 number,
+    c3 number
+);
+
+alter table c5 rename column c3 to name;
+
+alter table c5
+    modify name varchar(20);
+
+alter table c5
+    modify name number;
+
+-- column add
+
+alter table c5
+    add (c4 number,c5 varchar2(30));
+
+drop table departments;
+
+create table departments
+(
+    department_id   number(4),
+    department_name varchar2(30),
+    manager_id      number(4)
+);
+
+alter table departments
+    add(location_id number(4));
+
+
+alter table departments
+    modify department_id constraint dep_id_pk primary key;
+
+drop table departments;
+
+drop table EMPLOYEES;
+
+
+create table employees
+(
+    employee_id    number(6),
+    first_name     varchar2(20),
+    last_name      varchar2(25),
+    email          varchar2(25),
+    phone_number   varchar2(20),
+    hire_date      date,
+    job_id         varchar2(10),
+    salary         number(8, 2),
+    commission_pct number(2, 2),
+    manager_id     number(6),
+    department_id  number(4)
+);
+
+alter table employees
+add constraint emp_empid_pk primary key (employee_id);
+
+drop table JOB_HISTORY;
+create table JOB_HISTORY(
+    employee_id number(6),
+    start_date date ,
+    end_date date not null ,
+    job_id varchar2(10) not null ,
+    department_id number(4),
+    constraint job_history_pk primary key(employee_id,start_date)
+);
+
+drop table jobs;
+create table JOBS(
+    job_id varchar2(10) constraint job_jobs_pk primary key ,
+    job_title varchar2(35),
+    Min_salary number(6),
+    max_salary number(6)
+);
+
+alter table JOB_HISTORY
+add constraint job_jobid_fk foreign key (job_id) references JOBS(job_id);
+drop table departments;
+create table departments(
+    department_id number(4) primary key ,
+    department_name varchar2(30),
+    manager_id number(6),
+    location_id number(4)
+);
+
+select * from departments;
